c735aaf1cd5965c1d55d0dc9761b914b0af58a73
Paul Guignard <p.guignard@gmail.com>
2025-08-21 13:05:50 +0200

    website:feat: Create React empty site issue #3

---
+++ website/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>ThenElse</h1>
+      <p>Bienvenue sur votre application React</p>
+    </div>
+  )
+}
+
+export default App
